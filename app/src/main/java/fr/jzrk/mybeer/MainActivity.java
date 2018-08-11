623bfe9acafcef95a19d43c42823df0a57ad428d
JZrk <julie.zurek@outlook.com>
2018-08-11 16:31:51 +0200

    Création d'un input EditText avec listener

--- app/src/main/java/fr/jzrk/mybeer/MainActivity.java
+++ app/src/main/java/fr/jzrk/mybeer/MainActivity.java
@@ -2,6 +2,11 @@ package fr.jzrk.mybeer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mainText = this.findViewById(R.id.main_text_input);
+        EditText editText = this.findViewById(R.id.plain_text_input);
+        editText.setText("Do you <3 beer ?");
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mainText.setText(s);
+            }
+        });
+
+
     }
 }
